4ab373d351c8918a29d70723455d8920e0afe840
anhnmt <anhng.dev@gmail.com>
2023-10-21 23:00:10 +0700

    Update format, comment lab1_tl1

--- lab1/lab1_tl1/src/main.rs
+++ lab1/lab1_tl1/src/main.rs
@@ -6,17 +6,14 @@ fn main() {
 
     print!("Nhap n = ");
 
-    //
+    // https://stackoverflow.com/questions/34993744/why-does-this-read-input-before-printing
     let _ = io::stdout().flush();
 
-    if let Err(err) = io::stdin()
-        .read_line(&mut num) {
+    if let Err(err) = io::stdin().read_line(&mut num) {
         println!("Khong the nhan duoc du lieu: {}", err);
     }
 
-    let n: i64 = num
-        .trim()
-        .parse().expect("Khong phai so nguyen");
+    let n: i64 = num.trim().parse().expect("Khong phai so nguyen");
 
     let result: i64 = n * n;
     println!("Binh phuong n = {}", result);
