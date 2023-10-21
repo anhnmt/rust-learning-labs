fd3e527a00e34789daece4298b49d928972ddc27
anhnmt <anhng.dev@gmail.com>
2023-10-21 23:29:45 +0700

    Update lab1_tl2

---
+++ lab1/lab1_tl2/src/main.rs
@@ -0,0 +1,18 @@
+use std::io::Write;
+
+fn main() {
+    let mut input = String::new();
+
+    print!("Nhap ban kinh hinh tron = ");
+    let _ = std::io::stdout().flush();
+
+    std::io::stdin().read_line(&mut input).expect("Khong doc duoc");
+
+    let r: f64 = input.trim().parse().expect("Khong phai so nguyen");
+
+    let d = 3.14 * (r * r);
+    println!("Dien tich hinh tron = {}", d);
+
+    let c = 3.14 * (r * 2.0);
+    println!("Chu vi hinh tron = {}", c);
+}
